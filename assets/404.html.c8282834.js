7a8e30ffd83703958eea93c92ca69c6b34f92da2
william89turner <william.turner.89@mail.ru>
2022-04-05 18:19:40 +0800

    deploy web : V0.02:update

---
+++ assets/404.html.c8282834.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.7ff3558d.js";const _={};function e(t,c){return null}var a=r(_,[["render",e]]);export{a as default};
---
+++ assets/welcome.html.8b846fc1.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Wagmi.Fan",lang:"en-US",frontmatter:{index:1,title:"Welcome to Wagmi.Fan",author:"Wagmi.Fan",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Wagmi Fan : Fans of WagMi(We\u2018re all going to make it).
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://wagmi.fan/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Wagmi.Fan"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Wagmi.Fan"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Wagmi Fan : Fans of WagMi(We\u2018re all going to make it).</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Wagmi Fan",slug:"wagmi-fan",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why WagmiFan?",slug:"why-wagmifan",children:[]},{level:2,title:"Tech Stack to Build Wagmi Fan",slug:"tech-stack-to-build-wagmi-fan",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649149708e3,updatedTime:1649149708e3,contributors:[{name:"william89turner",email:"william.turner.89@mail.ru",commits:1}]},readingTime:{minutes:.47,words:141},filePathRelative:"welcome.md"};export{e as data};
